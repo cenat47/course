83bc9fa97122e04f9f8c6d9886b777bee5951c0c
cenat47 <000099909909a@gmail.com>
2025-09-05 21:13:56 +0300

    Задание 3

---
+++ src/models/hotels.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database import Base
+from sqlalchemy import String
+
+class HotelsOrm(Base):
+    __tablename__ = "Hotels"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    location: Mapped[str]
---
+++ src/models/rooms.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database import Base
+from sqlalchemy import String, ForeignKey
+
+class RoomsOrm(Base):
+    __tablename__ = "Rooms"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("Hotels.id"))
+    title: Mapped[str]
+    description: Mapped[str | None]
+    price: Mapped[int]
+    quantity: Mapped[int]
